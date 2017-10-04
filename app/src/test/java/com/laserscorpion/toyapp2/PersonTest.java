c12341c4858038ea8d84019eade974286b8a44a5
Joel Cretan <jcretan@gmail.com>
2017-10-03 23:08:21 -0400

    adding another test

--- app/src/test/java/com/laserscorpion/toyapp2/PersonTest.java
+++ app/src/test/java/com/laserscorpion/toyapp2/PersonTest.java
@@ -16,4 +16,14 @@ public class PersonTest {
         person1.id = 25;
         assertEquals(person1, person1);
     }
+
+    @Test
+    public void personDoesntEqualSomeoneElse() {
+        Person person1 = new Person();
+        Person person2 = new Person();
+        person1.id = 25;
+        person2.id = 100;
+        assertNotEquals(person1, person2);
+    }
+
 }
