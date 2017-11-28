a28422e5a7589c1fb89ea2603d84f1f9e96f3533
sarangkarpate <karpatesj@gmail.com>
2017-11-28 17:48:12 -0500

    Fixing spaces and variable names
    from pylint suggestions

--- Impromptu/dbAdapter.py
+++ Impromptu/dbAdapter.py
@@ -3,15 +3,16 @@ import os
 import flask_pymongo
 
 def connect(app):
-	mydir = os.path.dirname(os.path.abspath(__file__))
-	with open(mydir+'/config/dbConfig.json') as json_data:
-		dbConfig = json.load(json_data)
+    mydir = os.path.dirname(os.path.abspath(__file__))
+    with open(mydir+'/config/dbConfig.json') as json_data:
+        db_config = json.load(json_data)
 
-	app.config['MONGOMLAB_HOST'] = dbConfig['MONGOMLAB_HOST']
-	app.config['MONGOMLAB_PORT'] = dbConfig['MONGOMLAB_PORT']
-	app.config['MONGOMLAB_DBNAME'] = dbConfig['MONGOMLAB_DBNAME']
-	app.config['MONGOMLAB_USERNAME'] = dbConfig['MONGOMLAB_USERNAME']
-	app.config['MONGOMLAB_PASSWORD'] = dbConfig['MONGOMLAB_PASSWORD']
-	mongo_mlab = flask_pymongo.PyMongo(app, config_prefix='MONGOMLAB')
+    app.config['MONGOMLAB_HOST'] = db_config['MONGOMLAB_HOST']
+    app.config['MONGOMLAB_PORT'] = db_config['MONGOMLAB_PORT']
+    app.config['MONGOMLAB_DBNAME'] = db_config['MONGOMLAB_DBNAME']
+    app.config['MONGOMLAB_USERNAME'] = db_config['MONGOMLAB_USERNAME']
+    app.config['MONGOMLAB_PASSWORD'] = db_config['MONGOMLAB_PASSWORD']
+    mongo_mlab = flask_pymongo.PyMongo(app, config_prefix='MONGOMLAB')
 
-	return mongo_mlab
+    return mongo_mlab
+    
