99d934eb45bc7cda16a17f57608d30463fbe05ae
Tahmid Munat <tmunat@gm.slc.edu>
2017-11-28 23:51:35 -0500

    create activity added; implementation incomplete

---
+++ app/src/main/java/com/laserscorpion/impromptu/CreateEventActivity.java
@@ -0,0 +1,36 @@
+package com.laserscorpion.impromptu;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.text.Layout;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.*;
+
+public class CreateEventActivity extends FragmentActivity {
+
+}
